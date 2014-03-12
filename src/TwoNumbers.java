726af7c5f8f573c079d93b71cb9fb0beeb0dc973
armentae <ernextoa@gmail.com>
2014-03-11 20:30:33 -0700

    First Commit

---
+++ src/TwoNumbers.java
@@ -0,0 +1,31 @@
+//TwoNumbers.java
+//Input-printing program.
+import java.util.Scanner; //program uses class Scanner
+
+public class TwoNumbers
+{
+ //main method begins execution of java application
+ public static void main (  String[] args  )
+ {
+ // create Scanner to obtain input from the command window
+ Scanner input = new Scanner(System.in);
+ 
+ int number1; //first number to display
+ int number2; //second number to display
+ String word = "and";
+ 
+ System.out.print("Enter one number: ");
+ number1 = input.nextInt(); //first number entered
+
+ System.out.print("Enter second number: ");
+ number2 = input.nextInt(); //second number entered
+
+ System.out.printf("Hello, you entered: %d %s %d", number1, word, number2);
+ System.out.println();
+ System.out.print("The sum of your numbers is:  ");
+ System.out.printf("%s ", ( number1) + (number2));
+ System.out.println();
+ System.out.print("The subtraction of your numbers is:  ");
+ System.out.printf("%s ", ( number1) - (number2));
+ }//end method main
+}//end class Input
